49f2def3c64e1be063c02dc2daa70c5ec384e769
RabbitMask <37649548+rabbitmask@users.noreply.github.com>
2019-06-27 16:10:49 +0800

    Add files via upload

---
+++ poc/CVE_2014_4210.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+'''
+ ____       _     _     _ _   __  __           _    
+|  _ \ __ _| |__ | |__ (_) |_|  \/  | __ _ ___| | __
+| |_) / _` | '_ \| '_ \| | __| |\/| |/ _` / __| |/ /
+|  _ < (_| | |_) | |_) | | |_| |  | | (_| \__ \   < 
+|_| \_\__,_|_.__/|_.__/|_|\__|_|  |_|\__,_|___/_|\_\
+
+'''
+import logging
+import sys
+import requests
+
+logging.basicConfig(filename='Weblogic.log',
+                    format='%(asctime)s %(message)s',
+                    filemode="w", level=logging.INFO)
+
+headers = {'user-agent': 'ceshi/0.0.1'}
+
+def islive(ur,port):
+    url='http://' + str(ur)+':'+str(port)+'/uddiexplorer/'
+    r = requests.get(url, headers=headers)
+    return r.status_code
+
+def run(url,port):
+    if islive(url,port)==200:
+        u='http://' + str(url)+':'+str(port)+'/uddiexplorer/'
+        logging.info('[+]The target Weblogic UDDI module is exposed! The path is: {} Please verify the SSRF vulnerability!'.format(u))
+        print('[+]The target Weblogic UDDI module is exposed!\n[+]The path is: {}\n[+]Please verify the SSRF vulnerability!'.format(u))
+    else:
+        logging.info("[-]The target Weblogic UDDI module default path does not exist!")
+        print("[-]The target Weblogic UDDI module default path does not exist!")
+
+if __name__=="__main__":
+    url = sys.argv[1]
+    port = int(sys.argv[2])
+    run(url,port)
---
+++ poc/CVE_2018_2894.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+'''
+ ____       _     _     _ _   __  __           _
+|  _ \ __ _| |__ | |__ (_) |_|  \/  | __ _ ___| | __
+| |_) / _` | '_ \| '_ \| | __| |\/| |/ _` / __| |/ /
+|  _ < (_| | |_) | |_) | | |_| |  | | (_| \__ \   <
+|_| \_\__,_|_.__/|_.__/|_|\__|_|  |_|\__,_|___/_|\_\
+
+'''
+import logging
+import sys
+import requests
+
+logging.basicConfig(filename='Weblogic.log',
+                    format='%(asctime)s %(message)s',
+                    filemode="w", level=logging.INFO)
+
+VUL=['CVE-2018-2894']
+headers = {'user-agent': 'ceshi/0.0.1'}
+
+def islive(ur,port):
+    url='http://' + str(ur)+':'+str(port)+'/ws_utc/resources/setting/options/general'
+    r = requests.get(url, headers=headers)
+    return r.status_code
+
+def run(url,port,index):
+    if islive(url,port)!=404:
+        logging.info('[+]The target weblogic has a JAVA deserialization vulnerability:{}'.format(VUL[index]))
+        print('[+]The target weblogic has a JAVA deserialization vulnerability:{}'.format(VUL[index]))
+    else:
+        logging.info('[-]Target weblogic not detected {}'.format(VUL[index]))
+        print('[-]Target weblogic not detected {}'.format(VUL[index]))
+
+if __name__=="__main__":
+    url = sys.argv[1]
+    port = int(sys.argv[2])
+    run(url,port,0)
